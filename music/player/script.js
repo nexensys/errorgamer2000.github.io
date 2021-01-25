e8e19b812243445ae542505e7d86118b608733ff
ErrorGamer2000 <70162741+ErrorGamer2000@users.noreply.github.com>
2021-01-25 12:30:41 -0600

    Update script.js

--- music/player/script.js
+++ music/player/script.js
@@ -1,4 +1,4 @@
-var src = 0, files;
+var src, files;
 window.onload = function() {
   
   var file = document.getElementById("thefile");
@@ -68,11 +68,70 @@ audio.addEventListener('ended', loadNext)
 
 function loadNext() {
     src++
-    if (src > files.length - 1) {
-        src = 0
-    }
-    audio.src = ''
     audio.src = URL.createObjectURL(files[src]);
     audio.load();
     audio.play();
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+
+    var canvas = document.getElementById("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    var ctx = canvas.getContext("2d");
+
+    src.connect(analyser);
+    analyser.connect(context.destination);
+
+    analyser.fftSize = 256;
+
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+
+    var dataArray = new Uint8Array(bufferLength);
+
+    var WIDTH = canvas.width;
+    var HEIGHT = canvas.height;
+
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barHeight;
+    var x = 0;
+
+    function renderFrame() {
+      requestAnimationFrame(renderFrame);
+
+      x = 0;
+
+      analyser.getByteFrequencyData(dataArray);
+
+      ctx.fillStyle = "#000";
+      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      for (var i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        
+        var r = barHeight + (25 * (i/bufferLength));
+        var g = 250 * (i/bufferLength);
+        var b = 50;
+
+        ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
+        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+
+        x += barWidth + 1;
+      }
+    }
+
+    audio.play();
+    renderFrame();
   };
+window.onmousemove = logMouseMove;
+var mousePos;
+function logMouseMove(e) {
+	e = event || window.event;	
+	mousePos = { x: e.clientX, y: e.clientY };
+	if (Number(mousePos.y) < window.innerHeight - 100) {
+	    audio.style.display = "none"
+	} else {
+	    audio.style.display = "block"
+	}
+}
